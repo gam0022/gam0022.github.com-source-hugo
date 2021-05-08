01eb6fe8c7848d36dfc720c6b9c7d5d28314df40
gam0022 <gam0022@gmail.com>
2021-05-09 00:16:05 +0900

    deploy.batにコメントを追記

--- deploy.bat
+++ deploy.bat
@@ -1,22 +1,27 @@
+rem コミットメッセージ
 set msg="Build %date%%time%"
 
 pushd public
 
+rem publicディレクトリを掃除
 git fetch -p
 git checkout -B master origin/master
 
 popd
 
+rem HTMLを生成
 hugo017.exe
 
 pushd public
 
+rem HTML生成物をコミット
 git add .
 git commit -m %msg%
 git push origin HEAD
 
 popd
 
+rem ソースをコミット
 git add .
 git commit -m %msg%
 git push origin HEAD
