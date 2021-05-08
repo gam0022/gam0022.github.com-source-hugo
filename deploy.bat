e96970bf8c1861bbb5d0914027774b513fb37480
gam0022 <gam0022@gmail.com>
2021-05-09 01:08:54 +0900

    コメントはremではなくて::でも代用できるらしい

--- deploy.bat
+++ deploy.bat
@@ -1,27 +1,27 @@
-rem コミットメッセージ
+:: コミットメッセージ
 set msg="Build %date%%time%"
 
 pushd public
 
-rem publicディレクトリを掃除
+:: publicディレクトリを掃除
 git fetch -p
 git checkout -B master origin/master
 
 popd
 
-rem HTMLを生成
+:: HTMLを生成
 hugo017.exe
 
 pushd public
 
-rem HTML生成物をコミット
+:: HTML生成物をコミット
 git add .
 git commit -m %msg%
 git push origin HEAD
 
 popd
 
-rem ソースをコミット
+:: ソースをコミット
 git add .
 git commit -m %msg%
 git push origin HEAD
